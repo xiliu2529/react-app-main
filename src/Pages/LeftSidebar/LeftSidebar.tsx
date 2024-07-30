9b99f85871a1d7ab7e26077c1fa9d3c7785e19aa
xiliu <130113225+xiliu2529@users.noreply.github.com>
2024-07-30 10:13:29 +0900

    完成选择按钮对话框

--- src/Pages/LeftSidebar/LeftSidebar.tsx
+++ src/Pages/LeftSidebar/LeftSidebar.tsx
@@ -4,41 +4,101 @@ import icon from '../../img/icon.svg';
 import { useMyContext } from '../../contexts/MyContext'; // 导入上下文
 
 interface LeftSidebarProps {
-  onButtonClick: (buttonName: number) => void; 
+  onButtonClick: (buttonName: number) => void;
 }
 
 const LeftSidebar: React.FC<LeftSidebarProps> = ({ onButtonClick }) => {
   const { isHistoricalActive } = useMyContext(); // 获取上下文中的 alignment
   const { setbuttonName } = useMyContext();
-  const [isHovered, setIsHovered] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // 用于控制对话框的显示和隐藏
+
+  const handleIconClick = () => {
+    setIsOpen(!isOpen); // 切换对话框的显示状态
+  };
+
+  const handleClose = () => {
+    setIsOpen(false); // 关闭对话框
+  };
+
   const handleButtonClick = (buttonName: number) => {
     onButtonClick(buttonName);
-    setbuttonName(buttonName)
+    setbuttonName(buttonName);
   };
 
   return (
     <aside className="left-sidebar">
-      <span className="icon-container" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-        <img src={icon} alt="Icon" className="icon" onClick={() => handleButtonClick(0)}/>
-        {isHovered && (
+      <span className="icon-container" onClick={handleIconClick}>
+        <img src={icon} alt="Icon" className="icon" />
+        {isOpen && (
           <div className="button-container">
-          {isHistoricalActive ? (
+            {isHistoricalActive ? (
               <>
-                  <button className="sidebar-button" onClick={() => handleButtonClick(1)}>Button 1</button>
-                  <button className="sidebar-button" onClick={() => handleButtonClick(2)}>Button 2</button>
-                  <button className="sidebar-button" onClick={() => handleButtonClick(3)}>Button 3</button>
-                  <button className="sidebar-button" onClick={() => handleButtonClick(4)}>Button 4</button>
-                  <button className="sidebar-button" onClick={() => handleButtonClick(5)}>Button 5</button>
-                  <button className="sidebar-button" onClick={() => handleButtonClick(6)}>Button 6</button>
-                  <button className="sidebar-button" onClick={() => handleButtonClick(7)}>Button 7</button>
-                  <button className="sidebar-button" onClick={() => handleButtonClick(8)}>Button 8</button>
+                <button className="sidebar-button" onClick={() => handleButtonClick(1)}>
+                  <svg width="34" height="24" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="34" height="24" fill="#0E0E0E" />
+                  </svg>
+                  条件設定+グリッド+グラフ
+                </button>
+                <button className="sidebar-button" onClick={() => handleButtonClick(2)}>
+                  <svg width="34" height="24" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="34" height="24" fill="#0E0E0E" />
+                  </svg>
+                  グリッド+グラフ
+                </button>
+                <button className="sidebar-button" onClick={() => handleButtonClick(3)}>
+                  <svg width="34" height="24" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="34" height="24" fill="#0E0E0E" />
+                  </svg>
+                  条件設定+グリッド
+                </button>
+                <button className="sidebar-button" onClick={() => handleButtonClick(4)}>
+                  <svg width="34" height="24" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="34" height="24" fill="#0E0E0E" />
+                  </svg>
+                  グリッドのみ
+                </button>
+                <button className="sidebar-button" onClick={() => handleButtonClick(5)}>
+                  <svg width="34" height="24" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="34" height="24" fill="#0E0E0E" />
+                  </svg>
+                  条件設定+グラフ
+                </button>
+                <button className="sidebar-button" onClick={() => handleButtonClick(6)}>
+                  <svg width="34" height="24" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="34" height="24" fill="#0E0E0E" />
+                  </svg>
+                  グラフのみ
+                </button>
+                <button className="sidebar-button" onClick={() => handleButtonClick(7)}>
+                  <svg width="34" height="24" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="34" height="24" fill="#0E0E0E" />
+                  </svg>
+                  上下レイアウト仮
+                </button>
+                <button className="sidebar-button" onClick={() => handleButtonClick(8)}>
+                  <svg width="34" height="24" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="34" height="24" fill="#0E0E0E" />
+                  </svg>
+                  上下レイアウト仮2
+                </button>
               </>
-          ) : (
+            ) : (
               <>
-                  <button className="sidebar-button" onClick={() => handleButtonClick(9)}>Button 9</button>
-                  <button className="sidebar-button" onClick={() => handleButtonClick(10)}>Button 10</button>
+                <button className="sidebar-button" onClick={() => handleButtonClick(9)}>
+                  <svg width="34" height="24" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="34" height="24" fill="#0E0E0E" />
+                  </svg>
+                  Button 9
+                </button>
+                <button className="sidebar-button" onClick={() => handleButtonClick(10)}>
+                  <svg width="34" height="24" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="34" height="24" fill="#0E0E0E" />
+                  </svg>
+                  Button 10
+                </button>
               </>
-          )}
+            )}
+            <button id='close' onClick={handleClose}>とじる</button>
           </div>
         )}
       </span>
