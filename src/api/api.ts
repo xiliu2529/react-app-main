ed0fc711872f5b8832eaac263fded5ade69f43bb
xiliu <130113225+xiliu2529@users.noreply.github.com>
2024-09-03 17:58:45 +0900

    9.3api更新完成

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,8 +6,15 @@ const data = {
   types: "volumecurve_info"
 };
 
+
+const data1 = {
+	service_name: "qww",
+	mode: "PUT",
+	types: "volumecurve_info",
+  put_json_data: {test: "12312345"}
+}
+
 export const fetchAPI = async () => {
-  
   try {
     const response = await fetch('http://11.255.97.33/home/member/qww_dev/prod/userdata/load', {
       method: 'POST', 
@@ -16,11 +23,28 @@ export const fetchAPI = async () => {
       },
       body: JSON.stringify(data)
     });
-
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-
+    const result = await response.json(); 
+    return result;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error;
+  }
+};
+export const fetchAPI1 = async () => {
+  try {
+    const response = await fetch('http://11.255.97.33/home/member/qww_dev/prod/userdata/load', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data1)
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
     const result = await response.json(); 
     return result;
   } catch (error) {
